a23f4c724dc8a0c315e240159dee5049a9450c8f
Tatsunosuke Tobita <tatsunosuke.tobita@wacom.com>
2024-06-13 10:27:47 +0900

    test: change on a local branch
    Local branch, test, changed

--- test.c
+++ test.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-	int a = 1;
+	int c = 1;
 	b = 2;
 	
-	printf("Git ccommand training: %d and %d\n", a, b);
+	printf("Git ccommand training: %d and %d\n", c, b);
 	printf("Git rebase test\n");
 	
 	return 0;
